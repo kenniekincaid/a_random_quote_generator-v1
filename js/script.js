f5025bb5105158bf794c98b7cc59135a9af9a842
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-05 08:20:16 -0500

    Relocated Console entry and added return space after line 32

--- js/script.js
+++ js/script.js
@@ -1,56 +1,32 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/*** 
-  Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener 
-  below will be triggered, and it will call, or "invoke", the `printQuote` 
-  function. So do not make any changes to the line of code below this 
-  comment.
-***/
-
-document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+var quotes = [
+  {
+    quote: "Our heavenly Father has a thousand ways to provide for us, of which we know nothing.",
+    source: "Ellen White"
+  }, 
+    {
+    quote: "What You Do Speaks So Loudly that I Cannot Hear What You Say.",
+    source: "Ralph Waldo Emerson",
+    citation: "[RWEO] 1841, Essays [First Series] by Ralph Waldo Emerson, Essay IX: The Over-Soul, Page 236, James Munroe and Company, Boston, Massachusetts."
+  },
+    {
+    quote: "Life becomes easier and more beautiful when we can see the good in other people.",
+    source: "Roy Bennett"
+  },
+    {
+    quote: "Do one thing every day that scares you.",
+    source: "Eleanor Roosevelt"
+  },
+    {
+    quote: "The flower that blooms in adversity is the rarest and most beautiful of all.",
+    source: "Walt Disney Company, Mulan",
+    year: "1998"
+  },
+];
+console.log(quotes)
+function getRandomQuote(array) {
+  var quoteIndex = Math.floor(Math.random() * quotes.length);  
+  return quotes[quoteIndex];
+  } 
+var printQuote = function () {
+  var randomQuote = getRandomQuote()
+}
