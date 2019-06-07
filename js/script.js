222cfe688f84f3c91da4db1e8607bb63a15808a6
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-07 16:40:06 -0500

    Additions to JS...
    Added an alert on page load; unable to added bibleverse property to object; removed quotations from year
    added function that places quote intervals at 20 seconds with color change.

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 //I am shooting for an 'Exceeds Expectations' gradew with this submission.
 
+alert("Hello! You are special and the sky is the limit! May these quotes bring light to your day.");
+
 //An indexed array of quotes and other objects that will later be referenced at random and/or conditionally...
 var quotes = [
   {
     quote: "Our heavenly Father has a thousand ways to provide for us, of which we know nothing.",
     source: "Ellen White"
-  }, 
+    }, 
   {
     quote: "What You Do Speaks So Loudly that I Cannot Hear What You Say.",
     source: "Ralph Waldo Emerson",
     citation: "- Essays [First Series] by Ralph Waldo Emerson",
-    year: "1841."
+    year: 1841
   },
   {
     quote: "Life becomes easier and more beautiful when we can see the good in other people.",
@@ -38,7 +40,8 @@ var quotes = [
   },
   {
     quote: "Delight thyself in the Lord: and He shall give thee the desires of thine heart. Commit thy way unto the LORD; trust also in Him, and He shall bring it to pass.",
-    source: "Psalm 37:4-5"
+    source: "Psalm 37:4-5",
+    dailyverse: "Cick for Bible verse of the day! https://www.amazingfacts.org/bible-study/verse-of-the-day",
   },
   {
     quote: "The flower that blooms in adversity is the rarest and most beautiful of all.",
@@ -65,7 +68,6 @@ if (randomQuote.citation) { //The citation will appear if it's true.
 if (randomQuote.year) { //The year will appear if it's true.
     HTMLString += "<span class='year'>" + randomQuote.year + "</span>";
 }
-HTMLString += "</p>";
 document.getElementById('quote-box').innerHTML = HTMLString; //
 }
 printQuote(); //Executes appearance of quotes to page.
@@ -81,3 +83,11 @@ setInterval( //function that runs random colors from the color hex
     document.body.style.backgroundColor = "#"+randomColor; //changes the background to a random color
 }, 20000); //The number of seconds between random color changes (one second per 1000 milliseconds).
 //reference for the above codes is https://www.codespeedy.com/how-to-change-background-color-every-seconds-in-javascript/
+
+//This function changes the quotes every 20 seconds in sync with the color background change
+document.getElementById('loadQuote').addEventListener("click", printQuote, true);
+var intervalID = window.setInterval(myCallback, 20000);
+function myCallback() {
+  printQuote();
+}
+intervalID(window.setInterval(myCallack, 20000));
