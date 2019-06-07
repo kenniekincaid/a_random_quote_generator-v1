72396e529ce2e4848a4001e6235cca6c952e939a
Kendra Roberson <kendra.roberson@verizon.com>
2019-06-07 14:55:40 -0500

    Changes in HTML, JS, & CSS
    Removed commas and color change in css; added random color generator in js; replaced original line items in html after code experimentation.

--- js/script.js
+++ js/script.js
@@ -9,8 +9,8 @@ var quotes = [
   {
     quote: "What You Do Speaks So Loudly that I Cannot Hear What You Say.",
     source: "Ralph Waldo Emerson",
-    citation: "Essays [First Series] by Ralph Waldo Emerson.",
-    year: "1841"
+    citation: "- Essays [First Series] by Ralph Waldo Emerson",
+    year: "1841."
   },
   {
     quote: "Life becomes easier and more beautiful when we can see the good in other people.",
@@ -19,6 +19,26 @@ var quotes = [
   {
     quote: "Do one thing every day that scares you.",
     source: "Eleanor Roosevelt"
+  },
+  {
+    quote: "Someone is sitting in the shade today because someone planted a tree a long time ago.",
+    source: "Warren Buffett"
+  },
+  {
+    quote: "Try to be a rainbow in someone's cloud.",
+    source: "Maya Angelou"
+  },
+  {
+    quote: "The measure of who we are is what we do with what we have.",
+    source: "Vince Lombardi"
+  },
+  {
+    quote: "And we know that in all things God works for the good of those who love Him, who have been called according to His purpose.",
+    source: "Romans 8:28"
+  },
+  {
+    quote: "Delight thyself in the Lord: and He shall give thee the desires of thine heart. Commit thy way unto the LORD; trust also in Him, and He shall bring it to pass.",
+    source: "Psalm 37:4-5"
   },
   {
     quote: "The flower that blooms in adversity is the rarest and most beautiful of all.",
@@ -31,27 +51,33 @@ function getRandomQuote(quotes)
  {
   var quoteIndex = Math.floor(Math.random() * quotes.length);  //defines the parameters for appearance of quotes
   return quotes[quoteIndex]; //runs the quotes from the index above
-};
+}
 //console.log(getRandomQuote(quotes);
 
-function printQuote() //quotes will appear on the page
-{
+function printQuote() {//quotes will appear on the page
   var HTMLString = "";
   var randomQuote = getRandomQuote(quotes);
-  HTMLString += "<p class='quote'>" + randomQuote.quote + "</p>"; // the quote will appear first
-  HTMLString += "<p class='source'>" + randomQuote.source + "</p>"; // the source will appear afterwards
-//if citation is available or true, it will appear after the quote
-if (randomQuote.citation) { 
-    HTMLString += "<span class='citation'>" + randomQuote.citation + "</span>"
+    HTMLString += "<p class='quote'>" + randomQuote.quote + "</p>"; //the quote will appear first
+    HTMLString += "<p class='source'>" + randomQuote.source + "</p>"; //the source will appear afterwards
+if (randomQuote.citation) { //The citation will appear if it's true.
+    HTMLString += "<span class='citation'>" + randomQuote.citation + "</span>";
 }
-//if year is available or true, it will appear with the quote
-if (randomQuote.year) { 
-    HTMLString += "<span class='year'>" + randomQuote.year + "</span>"
+if (randomQuote.year) { //The year will appear if it's true.
+    HTMLString += "<span class='year'>" + randomQuote.year + "</span>";
 }
 HTMLString += "</p>";
 document.getElementById('quote-box').innerHTML = HTMLString; //
 }
-printQuote();
+printQuote(); //Executes appearance of quotes to page.
+//console.log(printQuote);
 
-//the codes will continue to loop as long as this remains false or as the page is reloaded with a click
+//The codes will continue to loop as long as this remains false or as the page is reloaded with a click.
 document.getElementById('loadQuote').addEventListener("click", printQuote, false); 
+
+//Creates the random color hex and sets the interval to a certain number of seconds.
+setInterval( //function that runs random colors from the color hex
+  function () {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16); //creates a random color hex
+    document.body.style.backgroundColor = "#"+randomColor; //changes the background to a random color
+}, 20000); //The number of seconds between random color changes (one second per 1000 milliseconds).
+//reference for the above codes is https://www.codespeedy.com/how-to-change-background-color-every-seconds-in-javascript/
